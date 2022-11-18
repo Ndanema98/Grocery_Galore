c330e05684c5e50077648f381b893f443984516c
Ndanema98 <davidnda26@yahoo.co.uk>
2022-11-18 21:40:55 +0000

    Feat: Add get_dailysales_data function to request data from user

--- run.py
+++ run.py
@@ -15,9 +15,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Grocery_Galore')
 
-sales = SHEET.worksheet('Dailysales')
+def get_dailysales_data():
+    """
+    Request daily sales data from users
+    """
+    print("Please enter the sales data from the past day")
+    print("Enter the daily sales data in this order: Apples, Oranges, Bananas, Avocado, Pears, Grapes, Mangos")
+    print("Input should be 7 numbers, seperated by commas.")
+    print("Example: 11,22,33,44,55,66,77\n")
 
-data = sales.get_all_values()
+    data_str = input("Enter your data here: ")
+    print(f"The data you have provided is {data_str}")
 
-print(data)
+
+get_dailysales_data()
+
 
